1a74deffaaedb37020b5302d4d2ee3739afaa47b
Serg Creevanose <sergeikrivonos@gmail.com>
2020-07-27 02:29:12 +0300

    Simple example

---
+++ NonLinearSystem.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+#include <openmind/omnn/math/System.h>
+using namespace omnn::math;
+
+int main(int argc, char **argv) {
+	cout << "Hello System:\n";
+
+	DECL_VA(a);
+	DECL_VA(b);
+	auto equation1 = a - 8 - b;
+	auto equation2 = a + b - 21;
+
+	cout << "Equation 1: " << equation1 << endl;
+	cout << "Equation 2: " << equation2 << endl;
+
+	System s;
+	s << equation1;
+	s << equation2;
+
+	auto solutions_a = s.Solve(a);
+	cout << "Solutions " << a << ':' << endl;
+	for(auto& solution: solutions_a)
+	    cout << '\t' << solution << endl;
+
+	return 0;
+}
