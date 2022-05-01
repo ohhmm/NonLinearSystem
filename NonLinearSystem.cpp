fe22c9c5eb9a85b9d2ba142180fc2f93c9428f32
Sergei Krivonos <sergeikrivonos@gmail.com>
2022-05-02 01:41:52 +0300

    11 or 7

--- NonLinearSystem.cpp
+++ NonLinearSystem.cpp
@@ -11,15 +11,16 @@ int main(int argc, char **argv) {
 	DECL_VA(a);
 	DECL_VA(b);
 	auto equation1 = a - 8 - b;
-	auto equation2 = a + b - 21;
+    auto $equation2 = a + b - 21;
 
 	cout << "Equation 1: " << equation1 << endl;
-	cout << "Equation 2: " << equation2 << endl;
+	cout << "Equation 2: " << $equation2 << endl;
 
 	System s;
 	s << equation1;
-	s << equation2;
+	s << $equation2;
 
+	cout << "\nSolving..." << endl;
 	auto solutions_a = s.Solve(a);
 	cout << "Solutions " << a << ':' << endl;
 	for(auto& solution: solutions_a)
@@ -28,15 +29,25 @@ int main(int argc, char **argv) {
 
     cout << "\n\nHello Features!\n";
     cout << "\n\nHello expressions!\n";
-    auto t = equation1.LogicAnd(equation2);
+    auto t = equation1.LogicAnd($equation2);
     cout << "Expression t: " << t << endl
-        << "\nHello Multivalue expressions!\n"
-        << "Expressed a from expression t is  a = " << t(a) << endl
+    << "\nHello Multivalue expressions!\n";
+    
+    auto $11_or_$7 = Valuable::MergeOr(11, 7);
+    
+    cout << "Expressed a from expression t is  a = " << t(a) << endl
         << "±1 = " << Valuable::MergeOr(1, -1) << endl
         << "±7 = " << Valuable::MergeOr(7, -7) << endl
-        << " 8 or -7 = " << Valuable::MergeOr(8, -7) << endl
+        << "\n 11 or 7  =  " << $11_or_$7 << endl
+        << endl
         ;
-    
+
+    cout << "Distinct values of $11_or_$7 are as follows:" << endl;
+    for(auto& value: $11_or_$7.Distinct()){
+        cout << ' ' << value;
+    }
+    cout<< endl
+    ;
 
 	return 0;
 }
